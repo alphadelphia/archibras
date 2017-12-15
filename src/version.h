468539a7ab56bd7d2fd976317dac75b3fca67524
YarkoL <yarko.dev@gmail.com>
2017-12-16 00:30:26 +0200

    first commit

---
+++ src/version.h
@@ -0,0 +1,53 @@
+// Copyright (c) 2012 The Bitcoin developers
+// Copyright (c) 2012-2013 The PPCoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BITCOIN_VERSION_H
+#define BITCOIN_VERSION_H
+
+#include <string>
+
+//
+// client versioning
+//
+
+// These need to be macro's, as version.cpp's voodoo requires it
+#define CLIENT_VERSION_MAJOR       0
+#define CLIENT_VERSION_MINOR       6
+#define CLIENT_VERSION_REVISION    4
+#define CLIENT_VERSION_BUILD       0
+
+static const int CLIENT_VERSION =
+                           1000000 * CLIENT_VERSION_MAJOR
+                         +   10000 * CLIENT_VERSION_MINOR 
+                         +     100 * CLIENT_VERSION_REVISION
+                         +       1 * CLIENT_VERSION_BUILD;
+
+extern const std::string CLIENT_NAME;
+extern const std::string CLIENT_BUILD;
+extern const std::string CLIENT_DATE;
+
+// Harmonian
+#define HARMONIAN_VERSION_MAJOR       0
+#define HARMONIAN_VERSION_MINOR       0
+#define HARMONIAN_VERSION_REVISION    0
+#define HARMONIAN_VERSION_BUILD       1
+
+static const int HARMONIAN_VERSION =
+                           1000000 * HARMONIAN_VERSION_MAJOR
+                         +   10000 * HARMONIAN_VERSION_MINOR
+                         +     100 * HARMONIAN_VERSION_REVISION
+                         +       1 * HARMONIAN_VERSION_BUILD;
+
+//
+// network protocol versioning
+//
+
+static const int PROTOCOL_VERSION = 60006;
+static const int MIN_PROTO_VERSION = 209;
+static const int CADDR_TIME_VERSION = 1;
+
+// BIP 0031, pong message, is enabled for all versions AFTER this one
+static const int BIP0031_VERSION = 60000;
+
+#endif
